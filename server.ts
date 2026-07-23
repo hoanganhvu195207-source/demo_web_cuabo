33f166b372270b98ddab85f31b7de3600bdfc49a
hoanganhvu195207-source <hoanganhvu195207@gmail.com>
2026-07-23 11:33:07 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -46,7 +46,7 @@ async function startServer() {
   await loadDatabaseState();
 
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json({ limit: "20mb" }));
 
